5a7375c9e1f33e365e6229b2118200d41d64e609
Eric Roy <royalmo6@gmail.com>
2020-05-04 18:03:01 +0200

    Fixed append to none bug.

--- functions.py
+++ functions.py
@@ -59,7 +59,8 @@ def send_status(json_answers):
     if mcustatus == "Got error":
         return json_answers["connection-error"]
     timestring = str(datetime.now())[0:-7].split(" ").append(json_answers["pc-stages"][int(mcustatus[0])])
-    return json_answers["pcstatus-answer"][0].format(timestring.append(json_answers["pcstatus-answer"][int(mcustatus[1]) + 1]))
+    timestring = timestring.append(json_answers["pcstatus-answer"][int(mcustatus[1]) + 1])
+    return json_answers["pcstatus-answer"][0].format(timestring)
 
 def action_pc(action = "shutdown", op = 3):
     data = wget_mcu("/telegram" + action, True).split("\n")
