d2fa3932ac677ef2c38017cbf0cde18ab810fa51
Eric Roy <royalmo6@gmail.com>
2020-05-04 19:49:10 +0200

    A missing '*' was giving me a headache!! :(

--- functions.py
+++ functions.py
@@ -60,7 +60,7 @@ def send_status(json_answers):
         return json_answers["connection-error"]
     timestring = str(datetime.now())[0:-7].split(" ")
     result = timestring + [json_answers["pc-stages"][int(mcustatus[0])], json_answers["pcstatus-answer"][int(mcustatus[1]) + 1]]
-    return json_answers["pcstatus-answer"][0].format(result)
+    return json_answers["pcstatus-answer"][0].format(*result)
 
 def action_pc(action = "shutdown", op = 3):
     data = wget_mcu("/telegram" + action, True).split("\n")
