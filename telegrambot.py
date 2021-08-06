72501e74d8c68d4d5e811b205098634616e37b7b
Eric Roy <royalmo6@gmail.com>
2021-08-06 19:29:11 +0200

    I hope not it works because I'm getting mad right now

--- telegrambot.py
+++ telegrambot.py
@@ -228,7 +228,7 @@ def send_notifications():
             if len(notification) > 1:
                 bot.sendMessage(user, json_answers["notification_pc"].format(json_answers["pc-stages"][int(notification[0])], notification[1]))
             elif notification in ["0", "1", "2"]:
-                bot.sendMessage(user, json_answers["notification_cam"].format(json_answers["cams"][notification]))
+                bot.sendMessage(user, json_answers["notification_cam"].format(json_answers["cams"][int(notification)]))
                 take_snapshot(bot, user, -1)
     jsonfile["notify"] = {}
     dump_json_file("allowed_users.json", jsonfile)
